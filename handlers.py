7cb4e7e206d8bcc8fa61e36a5728238e69b31f26
Andrea Cedraro <a.cedraro@gmail.com>
2015-03-31 10:27:36 +0200

    Add  /completion end-point

--- handlers.py
+++ handlers.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import jedi
 
 app = Flask( __name__ )
@@ -10,3 +10,25 @@ def healthy():
 @app.route( '/ready' )
 def ready():
     return jsonify()
+
+
+@app.route( '/completion', methods=['POST'] )
+def completion():
+    script = _GetJediScript( request.get_json() )
+    return jsonify( {
+        'completions': [
+            {
+                'name':        completion.name,
+                'description': completion.description,
+                'docstring':   completion.docstring()
+            } for completion in script.completions() ]
+        } )
+
+
+def _GetJediScript( request_data ):
+    filename = request_data[ 'filepath' ]
+    contents = request_data[ 'contents' ]
+    line     = request_data[ 'line_num' ]
+    column   = request_data[ 'column_num' ]
+
+    return jedi.Script( contents, line, column, filename )
--- tests/handlers_test.py
+++ tests/handlers_test.py
@@ -8,6 +8,18 @@ sys.path.insert( 0, os.path.join(
 from webtest import TestApp
 import handlers
 from nose.tools import ok_
+from hamcrest import ( assert_that, only_contains, all_of, has_key, has_items,
+                       has_entry )
+
+def CompletionEntry( name ):
+    return has_entry( 'name', name )
+
+
+def valid_completions():
+    return all_of( has_key( 'docstring' ),
+                   has_key( 'name' ),
+                   has_key( 'description' ) )
+
 
 def test_healthy():
     app = TestApp( handlers.app )
@@ -17,3 +29,22 @@ def test_healthy():
 def test_ready():
     app = TestApp( handlers.app )
     ok_( app.get( '/ready' ) )
+
+
+def test_completion():
+    app = TestApp( handlers.app )
+    dir_of_current_script = os.path.dirname( os.path.abspath( __file__ ) )
+    filepath = os.path.join( dir_of_current_script, 'fixtures', 'basic.py' )
+    request_data = {
+            'filepath': filepath,
+            'line_num': 7,
+            'column_num': 2,
+            'contents': open( filepath ).read()
+    }
+
+    completions = app.post_json( '/completion',
+                                 request_data ).json[ 'completions' ]
+
+    assert_that( completions, only_contains( valid_completions() ) )
+    assert_that( completions, has_items( CompletionEntry( 'a' ),
+                                         CompletionEntry( 'b' ) ) )
