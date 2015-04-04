426a51b3f1cacd23ffefd3b3cca3a434e14401bd
Andrea Cedraro <a.cedraro@gmail.com>
2015-04-04 22:22:15 +0200

    Use vendored library

--- handlers.py
+++ handlers.py
@@ -1,5 +1,14 @@
+import os
+import sys
+
+third_party_dir = os.path.join( os.path.dirname( __file__ ), 'third_party' )
+for folder in os.listdir( third_party_dir ):
+    sys.path.insert( 0, os.path.realpath( os.path.join( third_party_dir,
+                                                        folder ) ) )
+
 from flask import Flask, jsonify, request
 import jedi
+
 
 app = Flask( __name__ )
 
