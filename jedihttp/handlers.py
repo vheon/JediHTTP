f54a18fdfa25fa125aa6fe82366061a875f3b32f
Andrea Cedraro <a.cedraro@gmail.com>
2015-10-10 21:27:53 +0200

    Use plugin for exception handling

--- jedihttp/handlers.py
+++ jedihttp/handlers.py
@@ -17,11 +17,13 @@ import json
 import jedi
 import logging
 from jedihttp.logger_plugin import LoggerPlugin
+from jedihttp.exception_plugin import ExceptionPlugin
 
 
 logger = logging.getLogger( __name__ )
 app = bottle.Bottle( __name__ )
 app.install( LoggerPlugin( logger ) )
+app.install( ExceptionPlugin( logger ) )
 
 
 @app.post( '/healthy' )
@@ -36,63 +38,49 @@ def ready():
 
 @app.post( '/completions' )
 def completions():
-  try:
-    script = _GetJediScript( request.json )
-    return {
-             'completions': [ {
-               'name':        completion.name,
-               'description': completion.description,
-               'docstring':   completion.docstring(),
-               'module_path': completion.module_path,
-               'line':        completion.line,
-               'column':      completion.column
-             } for completion in script.completions() ]
-           }
-  except Exception as e:
-    message = str( e )
-    return bottle.HTTPError( 500, message, e )
+  script = _GetJediScript( request.json )
+  return {
+            'completions': [ {
+              'name':        completion.name,
+              'description': completion.description,
+              'docstring':   completion.docstring(),
+              'module_path': completion.module_path,
+              'line':        completion.line,
+              'column':      completion.column
+            } for completion in script.completions() ]
+          }
 
 
 @app.post( '/gotodefinition' )
 def gotodefinition():
-  try:
-    script = _GetJediScript( request.json )
-    return {
-             'definitions': [ {
-               'module_path':       definition.module_path,
-               'line':              definition.line,
-               'column':            definition.column,
-               'in_builtin_module': definition.in_builtin_module(),
-               'is_keyword':        definition.is_keyword,
-               'description':       definition.description,
-               'docstring':         definition.docstring()
-             } for definition in script.goto_definitions() ]
-           }
-  except Exception as e:
-    message = str( e )
-    logger.debug( 'Exception in /gotodefinition: {0}'.format( message ) )
-    return bottle.HTTPError( 500, message, e )
+  script = _GetJediScript( request.json )
+  return {
+            'definitions': [ {
+              'module_path':       definition.module_path,
+              'line':              definition.line,
+              'column':            definition.column,
+              'in_builtin_module': definition.in_builtin_module(),
+              'is_keyword':        definition.is_keyword,
+              'description':       definition.description,
+              'docstring':         definition.docstring()
+            } for definition in script.goto_definitions() ]
+          }
 
 
 @app.post( '/gotoassignment' )
 def gotoassignments():
-  try:
-    script = _GetJediScript( request.json )
-    return {
-             'definitions': [ {
-               'module_path': definition.module_path,
-               'line': definition.line,
-               'column': definition.column,
-               'in_builtin_module': definition.in_builtin_module(),
-               'is_keyword': definition.is_keyword,
-               'description': definition.description,
-               'docstring': definition.docstring()
-             } for definition in script.goto_assignments() ]
-           }
-  except Exception as e:
-    message = str( e )
-    logger.debug( 'Exception in /gotoassignment: {0}'.format( message ) )
-    return bottle.HTTPError( 500, message, e )
+  script = _GetJediScript( request.json )
+  return {
+            'definitions': [ {
+              'module_path': definition.module_path,
+              'line': definition.line,
+              'column': definition.column,
+              'in_builtin_module': definition.in_builtin_module(),
+              'is_keyword': definition.is_keyword,
+              'description': definition.description,
+              'docstring': definition.docstring()
+            } for definition in script.goto_assignments() ]
+          }
 
 
 @app.error()
