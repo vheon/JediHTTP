6e433c59348ed4c47c040efb50c891c4759bcee4
Andrea Cedraro <a.cedraro@gmail.com>
2015-10-11 22:30:05 +0200

    Add command-line argument for server host

--- jedihttp/__main__.py
+++ jedihttp/__main__.py
@@ -21,16 +21,17 @@ import handlers
 
 def ParseArgs():
   parser = argparse.ArgumentParser()
+  parser.add_argument( '--host', type = str, default = '127.0.0.1',
+                       help = 'server host' )
   parser.add_argument( '--port', type = int, default = 0,
-                       help = 'server port')
-
+                       help = 'server port' )
   return parser.parse_args()
 
 
 def Main():
   args = ParseArgs()
   waitress.serve( handlers.app,
-                  host = '127.0.0.1',
+                  host = args.host,
                   port = args.port )
 
 if __name__ == "__main__":
