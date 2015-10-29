32c026969da905b5bb5b47d8104591da753b0d2a
Andrea Cedraro <a.cedraro@gmail.com>
2015-10-29 23:16:04 +0100

    Remove unused import

--- jedihttp/handlers.py
+++ jedihttp/handlers.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from __future__ import absolute_import
+
 import jedi
 import logging
 import json
