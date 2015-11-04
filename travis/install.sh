db8b353d8db9276a51c7f09129a73e34de1a0210
Andrea Cedraro <a.cedraro@gmail.com>
2015-11-04 18:54:27 +0100

    Fix pyenv version

--- travis/install.sh
+++ travis/install.sh
@@ -19,7 +19,7 @@ set -e
 
 if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
   # install pyenv
-  git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+  git clone --branch=v20151103 --depth=1 https://github.com/yyuu/pyenv.git ~/.pyenv
   PYENV_ROOT="$HOME/.pyenv"
   PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
